86bd00f77786f7b726b8ff533bd4a463ab010761
Pablo Campos <pjcv89@gmail.com>
2020-03-02 19:20:49 +0100

    Update install_starspace.sh

--- install_starspace.sh
+++ install_starspace.sh
@@ -2,6 +2,7 @@
 wget https://dl.bintray.com/boostorg/release/1.63.0/source/boost_1_63_0.zip
 unzip boost_1_63_0.zip
 mv boost_1_63_0 /usr/local/bin
+rm boost_1_63_0.zip
 git clone https://github.com/facebookresearch/Starspace.git
 cd Starspace
 make && make query_predict && make query_nn
